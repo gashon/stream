9f3389c90765779fb8822a0ac82760117577294f
GashonHussein <gashon96@gmail.com>
2023-10-03 14:56:30 -0700

    fix: firebase lib error

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,10 +1,10 @@
 import admin from "firebase-admin";
 
 // must use require for json import typing
-const serviceAccount = require("../config/service-account-key.json");
+// const serviceAccount = require("../config/service-account-key.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  // credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://fir-2bbe6-default-rtdb.firebaseio.com",
 });
 
