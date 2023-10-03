b1ec4d8da2cf8355be95dde64a0574938d7e2f3b
GashonHussein <gashon96@gmail.com>
2023-10-03 15:33:24 -0700

    fix: initializing firebase app multiple times

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,9 +3,11 @@ import admin from "firebase-admin";
 // must use require for json import typing
 const serviceAccount = require("../config/service-account-key.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://fir-2bbe6-default-rtdb.firebaseio.com",
-});
+if (admin.apps.length === 0) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://fir-2bbe6-default-rtdb.firebaseio.com",
+  });
+}
 
 export { admin };
