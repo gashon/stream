3818e8721b0829fdbfc8f8759f9af9366e527d1c
GashonHussein <gashon96@gmail.com>
2023-10-04 00:13:55 -0700

    feat: add favorites logic + storage lib

--- src/features/posts/components/post-page.tsx
+++ src/features/posts/components/post-page.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 
 import storage from "@/lib/storage";
@@ -8,28 +8,52 @@ type Props = {
   posts: Post[];
 };
 
+const Post: FC<
+  Post & {
+    isStarred: boolean;
+  }
+> = ({ isStarred: initStarredState, ...post }) => {
+  const [isStarred, setIsStarred] = useState<boolean>(initStarredState);
+
+  const onStarClick = () => {
+    // TODO add append method to storage lib
+    storage.set<Favorites>(
+      "favorites",
+      isStarred
+        ? storage
+            .get<Favorites>("favorites")
+            ?.filter(({ post_id: id }) => id !== post.post_id) ?? []
+        : [...(storage.get<Favorites>("favorites") ?? []), post]
+    );
+
+    setIsStarred((prev) => !prev);
+  };
+
+  return (
+    <div className="my-5 w-full">
+      <p className="opacity-50">{new Date(post.created_at).toLocaleDateString()} </p>
+
+      <div className="flex flex-row items-center justify-between">
+        <p>{post.content}</p>
+
+        <div className="cursor-pointer" onClick={onStarClick}>
+          {isStarred ? <AiFillStar /> : <AiOutlineStar />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export const PostPage: FC<Props> = ({ posts }) => {
   const starredPostIds =
     storage.get<Favorites>("favorites")?.map(({ post_id }) => post_id) || [];
 
   return (
     <div className="w-full border-r pr-2">
-      {posts.map(({ post_id, content, created_at }) => {
-        const isStarred = starredPostIds.includes(post_id);
+      {posts.map((post) => {
+        const isStarred = starredPostIds.includes(post.post_id);
 
-        return (
-          <div key={`post:${post_id}`} className="my-5 w-full">
-            <p className="opacity-50">{new Date(created_at).toLocaleDateString()} </p>
-
-            <div className="flex flex-row items-center justify-between">
-              <p>{content}</p>
-
-              <div className="cursor-pointer">
-                {isStarred ? <AiFillStar /> : <AiOutlineStar />}
-              </div>
-            </div>
-          </div>
-        );
+        return <Post key={`post:${post.post_id}`} isStarred={isStarred} {...post} />;
       })}
     </div>
   );
