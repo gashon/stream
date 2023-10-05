79b3beefe679219805bff7c5b919b1cc68862ea3
GashonHussein <gashon96@gmail.com>
2023-10-05 15:39:00 -0700

    feat: style pin icon

--- src/features/posts/components/post-page-with-delete.tsx
+++ src/features/posts/components/post-page-with-delete.tsx
@@ -20,16 +20,20 @@ const PostComponent: FC<PostComponent> = ({ onDelete, onPin, post }) => {
     <div className="my-5  w-full border-b py-2 border-gray-500">
       <div className="flex justify-between">
         <p className="opacity-50 text-sm">{new Date(post.created_at).toDateString()} </p>
-        {post.priority === 1 && (
-          <p className="opacity-25 text-md font-semibold">Pinned</p>
-        )}
+        <div className="flex flex-row gap-2 items-center">
+          {post.priority === 1 && (
+            <p className="opacity-25 text-md font-semibold">Pinned</p>
+          )}
+          <div
+            className="cursor-pointer opacity-75"
+            onClick={() => onPin(post.post_id, post.priority == 0)}
+          >
+            <AiFillPushpin />
+          </div>
+        </div>
       </div>
       <div className="flex flex-col justify-between mt-1">
         <p className="text-md">{post.content}</p>
-
-        <div onClick={() => onPin(post.post_id, post.priority == 0)}>
-          <AiFillPushpin />
-        </div>
 
         <div className="flex justify-end mt-1">
           <div
