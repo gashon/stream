2b9bc8924cebfe89f18431159323e5edf571cafa
GashonHussein <gashon96@gmail.com>
2023-10-05 13:05:08 -0700

    feat: add inter font to PageLayout

--- src/layouts/page-layout.tsx
+++ src/layouts/page-layout.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { FC, ReactNode } from "react";
-import { AiFillStar } from "react-icons/ai";
+import { Inter } from "next/font/google";
 
 type Props = {
   children: ReactNode;
@@ -8,6 +8,8 @@ type Props = {
   bottomLinkHref: string;
   title: string;
 };
+
+const inter = Inter({ subsets: ["latin"] });
 
 const logWarningMessage = () =>
   console.log(
@@ -29,9 +31,10 @@ export const PageLayout: FC<Props> = ({
 }) => {
   logWarningMessage();
 
-  console.log("href", bottomLinkHref);
   return (
-    <main className="flex justify-center bg-zinc-900 w-screen min-h-screen text-white">
+    <main
+      className={`${inter.className} flex justify-center bg-zinc-900 w-screen min-h-screen text-white`}
+    >
       <div className="w-1/2 lg:11/12 mt-28">
         <h1 className="text-2xl mb-10">{title}</h1>
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
-import { Inter } from "next/font/google";
 import { AiFillStar } from "react-icons/ai";
-import Link from "next/link";
 
 import { PostsList } from "@/features";
 import { PageLayout } from "@/layouts";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function FavoritePostsPage() {
   return (
