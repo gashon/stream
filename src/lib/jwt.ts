a1a7b9c8c823a2114911713549455ae95d285d42
GashonHussein <gashon96@gmail.com>
2023-10-05 16:30:45 -0700

    fix: rmv build file

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,7 +1,6 @@
 import { sign, verify } from "jsonwebtoken";
 
 const secret: string = process.env.JWT_SECRET!;
-console.log("TOKEN", secret);
 
 export const createToken = (payload: object, expiresIn = undefined) => {
   return sign(payload, secret, expiresIn ? { expiresIn } : {});
