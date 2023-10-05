ef78418eaf831f2812551e1143206ac7f022db44
GashonHussein <gashon96@gmail.com>
2023-10-05 11:35:24 -0700

    feat: add warning msg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,20 @@ import { PostsContainer } from "@/features";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const logWarningMessage = () =>
+  console.log(
+    ` Hi! Please, if you are clearing localStorage, please read the warning below :) - Gashon
+    +----------------------------------------------------------------------------+
+    | WARNING:                                                                   |
+    | Clearing localStorage may cause undefined behavior. For safe data deletion,|
+    | please clear both your cookie and localStorage.                            |
+    +----------------------------------------------------------------------------+
+    `
+  );
+
 export default function Home() {
+  logWarningMessage();
+
   return (
     <PageLayout title="Stream.">
       <PostsContainer />
