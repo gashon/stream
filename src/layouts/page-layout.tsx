497e545ea93bda6332fa7cea0676a9c7e35e237e
GashonHussein <gashon96@gmail.com>
2024-01-09 17:03:49 -0800

    fix: responsive tw width

--- src/layouts/page-layout.tsx
+++ src/layouts/page-layout.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { FC, ReactNode } from "react";
 import { Roboto } from "next/font/google";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 type Props = {
   children: ReactNode;
@@ -25,7 +25,7 @@ const logWarningMessage = () =>
     | please clear both your cookie and localStorage.                            |
     | Thanks! - Gashon                                                           |
     +----------------------------------------------------------------------------+
-    `
+    `,
   );
 
 export const PageLayout: FC<Props> = ({
@@ -38,24 +38,24 @@ export const PageLayout: FC<Props> = ({
 
   return (
     <>
-    <main
-      className={`${roboto.className} flex justify-center bg-zinc-900 w-screen min-h-screen text-white`}
-    >
-      <div className="w-1/2 lg:11/12 mt-28">
-        <h1 className="text-2xl mb-10">{title}</h1>
+      <main
+        className={`${roboto.className} flex justify-center bg-zinc-900 w-screen min-h-screen text-white`}
+      >
+        <div className="w-10/12 lg:w-1/2 mt-28">
+          <h1 className="text-2xl mb-10">{title}</h1>
 
-        {children}
+          {children}
 
-        <section className="fixed bottom-10 right-10">
-          <Link href={bottomLinkHref}>
-            <div className="flex flex-row gap-2 justify-center items-center underline">
-              {BottomLinkComponent}
-            </div>
-          </Link>
-        </section>
-      </div>
-    </main>
-    <Analytics />
+          <section className="fixed bottom-10 right-10">
+            <Link href={bottomLinkHref}>
+              <div className="flex flex-row gap-2 justify-center items-center underline">
+                {BottomLinkComponent}
+              </div>
+            </Link>
+          </section>
+        </div>
+      </main>
+      <Analytics />
     </>
   );
 };
